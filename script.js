a563017d68a80b1ded9e681dd74ee7ad5103d0f9
pandeymamta <63756009+pandeymamta@users.noreply.github.com>
2020-05-30 12:53:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,8 +16,8 @@ var dc = {};
 var homeHtmlUrl = "snippets/home-snippet.html";
 var allCategoriesUrl =
   "https://davids-restaurant.herokuapp.com/categories.json";
-var categoriesTitleHtml = "snippets/categories-title-snippet.html";
-var categoryHtml = "snippets/category-snippet.html";
+var categoriesTitleHtml = "categories-title-snippet.html";
+var categoryHtml = "category-snippet.html";
 var menuItemsUrl =
   "https://davids-restaurant.herokuapp.com/menu_items.json?category=";
 var menuItemsTitleHtml = "snippets/menu-items-title.html";
